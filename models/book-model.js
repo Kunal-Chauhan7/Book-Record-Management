d72ddcc3e2108d9c2f860cd9ff145d83bade4bfd
Kunal Chauhan <kunalchauhanlion@gmail.com>
2023-06-25 02:28:53 +0530

    done making module

---
+++ models/book-model.js
@@ -0,0 +1,28 @@
+const moongoose = require("mongoose");
+const schema = moongoose.Schema;
+const bookSchema = new schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    author:{
+        type:String,
+        required:true,
+    },
+    genre:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type:String,
+        required:true,
+    },
+    publisher:{
+        type:String,
+        required:true,
+    },
+},
+{
+    timestamps : true,
+}
+);
